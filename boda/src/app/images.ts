8680c217cc54e00aca4c75f04d17f4f253811f5c
Cesar <cesar.luis91@gmail.com>
2023-07-15 17:20:37 +0200

    contact and photos

--- boda/src/app/images.ts
+++ boda/src/app/images.ts
@@ -1,1 +1,11 @@
-export var images = [ 'Javea.JPG','Yamaguchi.jpg','Atardecer_Malvarrosa.jpg','Plaza_Reina.jpg','Bosque_Cuenca.jpg','Altea.jpg','Vinaroz.jpg','Viveros.jpg','Granada.jpg' ]
+export var images = [
+  'Sanfermines.jpg',
+  'Atardecer_Malvarrosa.jpg',
+  'Yamaguchi.jpg',
+  'Javea.JPG',
+  'Bosque_Cuenca.jpg',
+  'rio.jpg',
+  'Viveros.jpg',
+  'Plaza_Reina.jpg',
+  'Granada.jpg',
+]
