738ced5a292b8f8e87d66dc65ead25ebe3bd12c2
Cesar Luis <cesar.luis91@gmail.com>
2023-11-12 15:26:06 +0100

    favicon + image

--- boda/src/app/images.ts
+++ boda/src/app/images.ts
@@ -1,11 +1,11 @@
 export var images = [
   'Sanfermines.jpg',
-  'Atardecer_Malvarrosa.jpg',
+  'hawaian.jpg',
+  'Bosque_Cuenca.jpg',
   'Yamaguchi.jpg',
   'Javea.JPG',
-  'Bosque_Cuenca.jpg',
   'rio.jpg',
-  'Viveros.jpg',
-  'Plaza_Reina.jpg',
   'Granada.jpg',
+  'Plaza_Reina.jpg',
+  'Viveros.jpg',
 ]
